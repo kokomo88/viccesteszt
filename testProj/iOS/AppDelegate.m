b50531dfa77756ea083d362b9c2ab493109a213a
kokomo88 <wcpapir10@freemail.hu>
2015-07-20 15:14:59 +0200

    bugfix

--- testProj/iOS/AppDelegate.m
+++ testProj/iOS/AppDelegate.m
@@ -31,8 +31,7 @@
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
-
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:9381/index.ios.bundle"];
   /**
    * OPTION 2
    * Load from pre-bundled file on disk. To re-generate the static bundle
